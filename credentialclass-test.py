20e5fd5c7e4fcbef98998fa897530ade9dd3cda3
Loisa-Kitakaya <loisakitakaya@gmail.com>
2020-01-27 11:00:03 +0300

    created a subclass TestCase and instantiated an object instance

---
+++ credentialclass-test.py
@@ -0,0 +1,37 @@
+# create test for class Users
+
+# imports
+import unittest
+from credentialclass import Credentials
+
+# create unittest subclass
+
+
+class TestForClassUsers(unittest.TestCase):
+
+    """setting up our test"""
+
+    def setUp(self):
+
+        # creating an object instance of class users
+        self.new_profile = Credentials("twitter", "loisaK", "loisa123")
+
+    """testing if user profile can be saved"""
+
+    def test_if_can_save(self):
+
+        # run test case
+        self.new_profile.save_user_information()
+        self.assertEqual(len(Credentials.profile_database), 1)
+
+    """clean up after test"""
+
+    def tearDown(self):
+
+        # deleting our object instances
+        Credentials.profile_database = []
+
+
+if __name__ == "__main__":
+
+    unittest.main()
