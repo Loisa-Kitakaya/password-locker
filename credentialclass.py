f6ff772fc5454cec5fccabd67a60ce39db7b53f6
Loisa-Kitakaya <loisakitakaya@gmail.com>
2020-01-27 11:20:02 +0300

    created methods to: read data from a text file; write data to a text file

--- credentialclass.py
+++ credentialclass.py
@@ -25,3 +25,27 @@ class Credentials:
     def delete_user_profile(self):
 
         Credentials.profile_database.remove(self)
+
+    """method to write list to a textfile"""
+
+    def write_user_profile_to_file(self):
+
+        # opening the file
+        file = open("userprofile.txt", "w")
+
+        file.write(str(Credentials.profile_database))
+
+        # closing the file
+        file.close()
+
+    """method to read list from a text file"""
+
+    def read_user_profile_from_file(self):
+
+        # opening the file
+        file = open("userprofile.txt", "r")
+
+        file.readline()
+
+        # closing the file
+        file.close()
