5863eb91464cd198045da31d72a9197b33d62b10
Loisa-Kitakaya <loisakitakaya@gmail.com>
2020-01-27 11:02:59 +0300

    created a method to save user profiles to a list: profile_database

--- credentialclass.py
+++ credentialclass.py
@@ -1,5 +1,6 @@
 # create class
 
+
 class Credentials:
 
     # database to store all user information
@@ -8,7 +9,13 @@ class Credentials:
     """method to initialize profiles"""
 
     def __init__(self, account, profile_name, profile_password):
-        
+
         self.account = account
         self.profile_name = profile_name
         self.profile_password = profile_password
+
+    """method to save user information"""
+
+    def save_user_profile(self):
+
+        Credentials.profile_database.append(self)
